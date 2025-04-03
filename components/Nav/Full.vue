00afb599ba990b58577716e32deacebf568eeb5e
Paul Brouwer <pbrouwer@offertevergelijker.nl>
2025-04-03 18:13:32 +0200

    updated and fixed istio for local

---
+++ components/Nav/Full.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sticky top-0 bg-white shadow-lg py-4 z-[999]">
+    <div class="container mx-auto flex flex-wrap justify-between gap-y-4 items-center">
+      <div class="bg-white">
+        <div class="container">
+          <img alt="test" class="h-10.5" src="public/offertevergelijker_logo.png"/>
+        </div>
+      </div>
+      <nav class="bg-white">
+        <ul class="flex gap-x-10">
+          <li>Energie & Duurzaam</li>
+          <li>Klussen & Verbouwen</li>
+          <li>Verzekeringen</li>
+        </ul>
+      </nav>
+      <div class="bg-white">
+        <div class="flex gap-x-4">
+          <Button label="Inschrijven als vakman" type="quaternary"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,16 @@
 
 export default defineNuxtConfig({
     extends: ['../_common'],
-    components: ['../_common/components'],
     devtools: {enabled: true},
     future: {
         compatibilityVersion: 4,
     },
     compatibilityDate: '2024-04-03',
-    content: {
-        preview: {
-            api: 'https://api.nuxt.studio',
-            gitInfo: {
-                name: 'ov-site',
-                owner: 'offertevergelijker-nl',
-                url: 'https://github.com/offertevergelijker-nl/ov-site'
-            },
-        },
-    },
 
     // Styling
     css: ['./css/tailwind.css'],
+    modules: ['@nuxt/image'],
+    plugins: [
+        //
+    ]
 })
